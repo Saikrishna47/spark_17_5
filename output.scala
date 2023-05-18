06890248be96a8921df5553ec1f9ffbd95e78d6f
Saikrishna47 <30722407+Saikrishna47@users.noreply.github.com>
2023-05-18 17:57:14 +0530

    Update output.scala

--- output.scala
+++ output.scala
@@ -12,19 +12,23 @@ object matching
                                       structField("Price",String)
                                       ))
                                    
-                                      
+    //reading the file and storing into dataframe                                  
     val dataDF= spark.read.format("com.databricks.spark.csv").schema("dataSchema")
     
-    val selfjoinDF = dataDF.as("df1").join(dataDF.as("df1"), col("df1.OrderType")===col("df2.OrderType") && col("df1.Quantity")===col("df2.Quantity") )
-    .select("df1.OrderID","df1.OrderTime","df1.OrderType","df1.Quantity","df1.Price")
+    //get only records with both buy and sell orders 
+    val selfjoinDF = dataDF.as("df1").join(dataDF.as("df1"), col("df1.Quantity")===col("df2.Quantity") )
+    .select("df1.OrderID","df1.OrderTime","df1.OrderType","df1.Quantity","df1.Price").distinct()
     
+    //to check count in next steps for orders, ranking over orderid
     val rankDF = selfjoinDF.withcolumn("rank", row_number().over(window.partitionBy("OrderID").orderBy("Quantity"))).select("*")
     
-    var orderDF= rankDF.groupby("OrderID","OrderType").agg(count("rank")).withcolumn("orderbook", where(count>1))
+    //grouping to get the status of order 
+    var orderDF= rankDF.groupby("rank","Quantity").agg(count("rank")).withcolumn("orderbook", when(col("agg")=2),lit("Closed")).otherwise(lit("Open")) )
     .select("OrderID","rank","OrderTime","Quantity","Price","orderbook","OrderType")
     
-    orderDF=orderDF.withcolumn("orderbook",where(col("OrderType")!="1",lit("open")))
+    //orderDF=orderDF.withcolumn("orderbook",where(col("OrderType")!="1",lit("open")))
     
+    //get final prices of orders placed
     var finalPriceDF= orderDF.filter(col("OrderType")=="BUY").groupby("OrderID","OrderType").min("price").withcolumnrenamed("min","finalPrice").select("*")
    
     finalPriceDF.filter(col("OrderType")=="SELL").groupby("OrderID","OrderType").max("price").withcolumnrenamed("max","finalPrice").select("*")
