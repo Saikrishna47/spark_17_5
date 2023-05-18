673ff4bcf0e1edefd343ebbb39ee8458049cc7d0
Saikrishna47 <30722407+Saikrishna47@users.noreply.github.com>
2023-05-18 10:37:13 +0530

    Update output.scala

--- output.scala
+++ output.scala
@@ -15,6 +15,22 @@ object matching
                                       
     val dataDF= spark.read.format("com.databricks.spark.csv").schema("dataSchema")
     
+    val selfjoinDF = dataDF.as("df1").join(dataDF.as("df1"), col("df1.OrderType")===col("df2.OrderType") && col("df1.Quantity")===col("df2.Quantity") )
+    .select("df1.OrderID","df1.OrderTime","df1.OrderType","df1.Quantity","df1.Price")
     
+    val rankDF = selfjoinDF.withcolumn("rank", row_number().over(window.partitionBy("OrderID").orderBy("Quantity"))).select("*")
+    
+    var orderDF= rankDF.groupby("OrderID","OrderType").agg(count("rank")).withcolumn("orderbook", where(count>1))
+    .select("OrderID","rank","OrderTime","Quantity","Price","orderbook","OrderType")
+    
+    orderDF=orderDF.withcolumn("orderbook",where(col("OrderType")!="1",lit("open")))
+    
+    var finalPriceDF= orderDF.when(col("OrderType")=="BUY").groupby("OrderID","OrderType").min("price").withcolumnrenamed("min","finalPrice").select("*")
+   
+    finalPriceDF.when(col("OrderType")=="SELL").groupby("OrderID","OrderType").max("price").withcolumnrenamed("max","finalPrice").select("*")
+    
+    val ouputDF = finalPriceDF.withcolumn("rank", row_number().over(window.partitionBy("OrderID").orderBy("OrderType"))).distinct("OrderTime")
+    .select("OrderID","rank","OrderTime","Quantity","finalPrice")
+
   }
 }
